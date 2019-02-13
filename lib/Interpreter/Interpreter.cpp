d4402c38e1232f892353f097175066c85e485bb0
Simeon Ehrig <s.ehrig@hzdr.de>
2019-02-13 14:57:18 +0100

    Setting a new include path at runtime in the PTX compiler now works

--- lib/Interpreter/Interpreter.cpp
+++ lib/Interpreter/Interpreter.cpp
@@ -649,6 +649,9 @@ namespace cling {
                               E.Group == frontend::Angled);
       }
     }
+
+    if (m_CUDACompiler)
+      m_CUDACompiler->getPTXInterpreter()->AddIncludePaths(PathStr, Delm);
   }
 
   void Interpreter::AddIncludePath(llvm::StringRef PathsStr) {
